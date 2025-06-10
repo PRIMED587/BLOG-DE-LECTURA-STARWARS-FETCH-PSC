bc1d9484eac51b892db1cc1e6b810ec901a42cf8
Pascual Rivera M <priveram87@gmail.com>
2025-06-10 21:57:54 +0000

    revision

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,26 +1,25 @@
-import { Outlet } from "react-router-dom/dist"
-import ScrollToTop from "../components/ScrollToTop"
-import Navbar from "../components/Navbar"
-import { Footer } from "../components/Footer"
-import SideBar from "../components/SideBar"
+// Layout.jsx (actualizado para mejor responsividad)
+import { Outlet } from "react-router-dom";
+import ScrollToTop from "../components/ScrollToTop";
+import Navbar from "../components/Navbar";
+import { Footer } from "../components/Footer";
+import SideBar from "../components/SideBar";
 
-// Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
 export const Layout = () => {
     return (
         <ScrollToTop>
             <Navbar />
-            <div className="container">
+            <div className="container-fluid">
                 <div className="row">
-                    <div className="col-md-3">
+                    <div className="col-12 col-md-3 mb-3">
                         <SideBar />
                     </div>
-                    <div className="col-md-9">
+                    <div className="col-12 col-md-9">
                         <Outlet />
-
                     </div>
                 </div>
             </div>
             <Footer />
         </ScrollToTop>
-    )
-}
+    );
+};
